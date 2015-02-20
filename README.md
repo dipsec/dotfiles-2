a1373599f8560ddde4284fd7e6032c59537549e4
Chris Lane <chris@chris-allen-lane.com>
2015-02-19 22:58:58 -0500

    Moving stuff around

--- README.md
+++
@@ -1,3 +0,0 @@
-dotfiles
-========
-These are my dotfiles. They can be deployed using the `rake` utility. Run `rake -T` for more information.
--- awm/rc.lua
+++ awm/rc.lua
@@ -73,10 +73,15 @@ tags  = {}
 -- Presuming that three monitors are detected (which is expected), give
 -- different tags to each
 if screen.count() == 4 then
-    tags[1] = awful.tag({ '1.trm  ' , '2.trm  ' , '3.trm  ' , '4.www  ' , '5.etc' },            1 , layouts[1])
-    tags[2] = awful.tag({ '1.www  ' , '2.www  ' , '3.www  ' , '4.www  ' , '5.soc' },            2 , layouts[1])
-    tags[3] = awful.tag({ '1.wrt  ' , '2.wrt  ' , '3.dev  ' , '4.dev  ' , '5.dev  ', '6.etc' }, 3 , layouts[1])
-    tags[4] = awful.tag({ '1.com  ' , '2.med  ' , '3.btc  ' , '4.etc  ' , '5.vol' },            4 , layouts[1])
+    tags[1] = awful.tag({ '1.trm  '  , '2.trm  '  , '3.trm  ' , '4.www  ' , '5.etc' },                        1 , layouts[1])
+    tags[2] = awful.tag({ '1.www  '  , '2.www  '  , '3.www  ' , '4.www  ' , '5.soc' },                        2 , layouts[1])
+    tags[3] = awful.tag({
+                          '1.wrt  '  , '2.wrt  '  , '3.wrt  ' ,
+                          '4.dev  '  , '5.dev  '  , '6.dev  ' ,
+                          '7.wrk  '  , '8.wrk  '  , '9.wrk  ' , '10.wrk  ' ,
+                          '11.etc'
+    }, 3 , layouts[1])
+    tags[4] = awful.tag({ '1.com  '  , '2.med  '  , '3.btc  ' , '4.etc  ' , '5.vol' },                        4 , layouts[1])
 
 -- if a number of monitors other than three are detected, apply a generic default
 else
